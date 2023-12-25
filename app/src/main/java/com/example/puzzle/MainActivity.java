e83d65640ba3493291379e2f8f2f0e6385a4485c
Pavel Kravchenko <pah595787@gmail.com>
2023-12-25 20:14:58 +0200

    Игровое поле

--- app/src/main/java/com/example/puzzle/MainActivity.java
+++ app/src/main/java/com/example/puzzle/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_start = findViewById(R.id.button_start);
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra("targetSound", "targetSound");
+                setResult(Activity.RESULT_OK, intent);
+                startActivityForResult(intent,101);
+                onNewIntent (intent);
+            }
+        });
     }
 }
